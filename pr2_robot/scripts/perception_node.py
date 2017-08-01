5ca5e8dcb9e69c86c14cf18f5efc8ea503593c74
Till Barmeier <tbarmeier@googlemail.com>
2017-08-01 21:59:17 +0200

    add clustering of detected objects

--- pr2_robot/scripts/perception_node.py
+++ pr2_robot/scripts/perception_node.py
@@ -88,14 +88,38 @@ def pcl_callback(pcl_msg):
     #pcl_table = pcl_data.extract(inliers, negative=False)
     pcl_objects = pcl_data.extract(inliers, negative=True)
 
-    # TODO: Euclidean Clustering
+    # Euclidean Clustering
+    white_objects = XYZRGB_to_XYZ(pcl_objects)
+    tree = white_objects.make_kdtree()
+    ec = white_objects.make_EuclideanClusterExtraction()
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(10)
+    ec.set_MaxClusterSize(2500)
+    ec.set_SearchMethod(tree)
+    cluster_indices = ec.Extract()
 
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Create Cluster-Mask Point Cloud to visualize each cluster separately
+    cluster_color = get_color_list(len(cluster_indices))
+    color_cluster_point_list = []
+    
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_objects[indice][0],
+                                            white_objects[indice][1],
+                                            white_objects[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+    
+    #Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
-    # TODO: Convert PCL data to ROS messages
+
+    # Convert PCL data to ROS messages
     ros_pcl_objects = pcl_to_ros(pcl_objects)
+    ros_pcl_clusters = pcl_to_ros(cluster_cloud)
     # TODO: Publish ROS messages
     pcl_objects_pub.publish(ros_pcl_objects)
+    cluster_test_pub.publish(ros_pcl_clusters)
 
 # Exercise-3 TODOs:
 
@@ -170,6 +194,7 @@ if __name__ == '__main__':
 
     # Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    cluster_test_pub = rospy.Publisher("/pcl_clusters", PointCloud2, queue_size=1)
 
     # TODO: Load Model From disk
 
